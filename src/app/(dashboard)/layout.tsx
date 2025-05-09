0d7b207db96c9d8f500bd32a33830d4424f39003
vially <viallymboma@users.noreply.github.com>
2025-05-09 06:23:33 +0100

    added authentication modules and system

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,17 +1,16 @@
 "use client";
 import React from 'react';
 
-import { ProtectedRoute } from '@/wrapper/ProtectedRoute';
-
+// import { ProtectedRoute } from '@/wrapper/ProtectedRoute';
 import DashboardLayout from './dashboard/DashboardLayout';
 
 const layout = ({ children }: { children: React.ReactNode }) => {
     return (
-        <ProtectedRoute >
+        // <ProtectedRoute >
+        // </ProtectedRoute>
             <DashboardLayout>
                 { children }
             </DashboardLayout>
-        </ProtectedRoute>
     )
 }
 
